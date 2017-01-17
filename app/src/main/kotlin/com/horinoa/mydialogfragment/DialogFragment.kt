d15feede2efabe46ee59aa38138571a1da11afcc
horinoA <horinoakiko@gmail.com>
2017-01-18 05:12:18 +0900

    とりあえず基礎完成

--- app/src/main/kotlin/com/horinoa/mydialogfragment/DialogFragment.kt
+++ app/src/main/kotlin/com/horinoa/mydialogfragment/DialogFragment.kt
@@ -1,26 +1,70 @@
 package com.horinoa.mydialogfragment
 
-import android.app.Dialog
+import android.app.*
 import android.app.DialogFragment
+import android.content.DialogInterface
 import android.os.Bundle
 
 /**
  * Created by horinoA on 2017/01/13.
  */
-open class DialogFragment : DialogFragment() {
+
+val TITLE = "title"
+val MESSE = "messe"
+
+
+interface DialogFragmentImpl{
+    fun OkAlertShow(titel:Int,messe:Int,callback:(Boolean)->Unit)
+}
+
+class DialogFragment {
+
+    fun OkAlertShow(activity:Activity,titel: Int, messe: Int, callback: (Boolean) -> Unit) {
+        val args = Bundle()
+        args.putInt(TITLE,R.string.end_messe_title)
+        args.putInt(MESSE,R.string.end_messe_messe)
+        val mydialogfragment = MyDialogFragment.newInstance(args)
+        mydialogfragment.setOnOkClickListener(
+                DialogInterface.OnClickListener { dialog, i ->
+                    callback(true)
+                    dialog.dismiss()
+                })
+        mydialogfragment.show(activity.fragmentManager,"")
+    }
+
+}
+
+open class MyDialogFragment : DialogFragment() {
+
+    var okClickListner : DialogInterface.OnClickListener? = null
 
     companion object{
-        fun newInstance(title:Int){
-
+        fun newInstance(getargs:Bundle): MyDialogFragment {
+            val dialogfragment = MyDialogFragment()
+            val args = Bundle()
+            args.putInt(TITLE,getargs.getInt(TITLE))
+            args.putInt(MESSE,getargs.getInt(MESSE))
+            dialogfragment.arguments = args
+            return dialogfragment
         }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
+        return AlertDialog.Builder(activity)
+                .setTitle(getString(arguments.getInt(TITLE)))
+                .setMessage(getString(arguments.getInt(MESSE)))
+                .setPositiveButton("ok",this.okClickListner)
+                .create()
     }
 
     override fun onPause() {
         super.onPause()
         dismiss()
     }
+
+    fun setOnOkClickListener(listener:DialogInterface.OnClickListener) {
+        this.okClickListner = listener
+    }
 }
+
+
--- app/src/main/kotlin/com/horinoa/mydialogfragment/MainActivity.kt
+++ app/src/main/kotlin/com/horinoa/mydialogfragment/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.horinoa.mydialogfragment
 
+import android.app.FragmentManager
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val okAlert:Button by lazy { findViewById(R.id.okAlertButtom) as Button }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        okAlert.setOnClickListener{
+            val dialogfragment = DialogFragment()
+            dialogfragment.OkAlertShow(this,R.string.end_messe_title,R.string.end_messe_messe,
+                    {it ->
+                        if(it == true){
+                            Log.v("TAG","true")
+                        }else{
+                            Log.v("TAG","false")
+                        }
+                    }
+            )
+        }
+
     }
 }
+
